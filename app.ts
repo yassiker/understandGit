7434e8bb9ac9366ea613dd2b61d5c111e8d8e401
Yassine Mhanna <yas.mhanna@gmail.com>
2017-11-02 16:50:43 +0100

    no message

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ function genericFunc<T>(argument: T): T[] {
 
 }
 
-console.log('Branch of Yassine')
+console.log('Master')
 var arrayFromString = genericFunc<string>("beep");
 console.log(typeof arrayFromString[0]);
 // learn
