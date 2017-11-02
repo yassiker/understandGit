c64bba2d4e25128171834a48c89bc44739b460a0
Yassine Mhanna <yas.mhanna@gmail.com>
2017-11-02 16:21:28 +0100

    master

--- app.ts
+++ app.ts
@@ -6,11 +6,10 @@ function genericFunc<T>(argument: T): T[] {
     // hello master
 }
 
-console.log('Master')
 var arrayFromString = genericFunc<string>("beep");
 console.log(typeof arrayFromString[0]);
 // learn
-console.log('Master')
+
 
 var arrayFromNumber = genericFunc(45);
 console.log(arrayFromNumber[0]);
