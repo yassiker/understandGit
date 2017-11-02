fe9d2c57ff419b38866f8f483ca10ef49bb935a0
Yassine Mhanna <yas.mhanna@gmail.com>
2017-11-02 16:14:01 +0100

    no message

--- app.ts
+++ app.ts
@@ -10,6 +10,8 @@ console.log('Master')
 var arrayFromString = genericFunc<string>("beep");
 console.log(typeof arrayFromString[0]);
 // learn
+console.log('Master')
+
 var arrayFromNumber = genericFunc(45);
 console.log(arrayFromNumber[0]);
 console.log(typeof arrayFromNumber[0])
